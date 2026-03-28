da969cb3b988a1643bb30b96942556d9373dabe9
Aakash Neupane <neupaneakash687@gmail.com>
2026-03-28 17:19:26 +0545

    chore: set Supabase anon public key

--- senior-cafe-restaurant-site/supabase-config.js
+++ senior-cafe-restaurant-site/supabase-config.js
@@ -3,7 +3,7 @@
 // `requireAuthForOrders: true` means users must sign in before placing/syncing orders.
 window.KanigiriSupabaseConfig = {
   url: "https://lxfwqeotuxjhwlojkhnl.supabase.co",
-  anonKey: "",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx4ZndxZW90dXhqaHdsb2praG5sIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ2OTA1ODQsImV4cCI6MjA5MDI2NjU4NH0.fuKOaRjEe79VyOjMZ7Mf3sij2mYVbLbAhTOcgEjj-SY",
   schema: "public",
   ordersTable: "orders",
   requireAuthForOrders: true
